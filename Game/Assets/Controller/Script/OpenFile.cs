395a07c2617a1b9f5c05a19e0326b4b398e03a2a
ajaxpawar <ajaypawar8762@gmail.com>
2022-03-18 20:32:33 +0530

    18 march toast

--- Game/Assets/Controller/Script/OpenFile.cs
+++ Game/Assets/Controller/Script/OpenFile.cs
@@ -1,12 +1,30 @@
-#if UNITY_EDITOR
-using UnityEditor;
-#endif 
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using EasyUI.Toast;
 
 public class OpenFile : MonoBehaviour
-{  public string selectFile()
+{
+    public string path = "";
+    public string selectFile()
     {
-        string path = EditorUtility.OpenFilePanel("Overwrite with mp3", "", "mp3");
+        //Toast.Show("Avalable in Windows Only...");
+
+#if UNITY_EDITOR
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            Toast.Show("Avalable in Windows Only...");
+        }
+        else
+        {
+            Toast.Show("Windows");
+            path = UnityEditor.EditorUtility.OpenFilePanel("Overwrite with mp3", "", "mp3");
+        }
+
+
+
+#endif
+
         return path;
     }
 }
